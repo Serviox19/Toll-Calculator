d57c4c9d9cbe05b81a84242c1d67f1d0dbb2d64e
Servio Mora <servio.mora19@gmail.com>
2016-05-22 15:35:14 -0400

    set up first part of geocoding address

--- public/angCtrl/app.js
+++ public/angCtrl/app.js
@@ -10,3 +10,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: 'public/views/partials/home.html'
   })
 });
+
+app.controller('mainController', function($scope, $http) {
+  $scope.getTollCost = function() {
+    var origin = $scope.origin;
+    var destination = $scope.destination;
+    $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='  + origin + '&key=AIzaSyA-nftHnip6wJ1zX_SoBfmmhZmynfvk-u0').then(function(data) {
+      var originGeoCode = data.data.results[0].geometry.location;
+      console.log(originGeoCode)
+    })
+  }
+});
